1f38db30a1de1ace0dd4ea3e943e92c30d93bca7
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-06 20:59:36 +0200

    Route-Trains association

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,6 @@
 class Train < ApplicationRecord
   validates :number, presence: true, uniqueness: true
+
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :route
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506183538) do
+ActiveRecord::Schema.define(version: 20170506185529) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -34,7 +34,9 @@ ActiveRecord::Schema.define(version: 20170506183538) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
+    t.integer  "route_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
 end

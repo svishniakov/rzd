b34ae9ce4a3c8a1ffdfe13ee8cdec7bf44a1c232
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-06 21:16:18 +0200

    User-Tickets association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,5 @@
 class Ticket < ApplicationRecord
   validates :number, presence: true, uniqueness: true
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506190412) do
+ActiveRecord::Schema.define(version: 20170506190950) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170506190412) do
     t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|

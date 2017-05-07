57f7517d20f34f17def78341fa8a2960593256ee
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-07 19:50:09 +0200

    Trains list using anonymous partials

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,6 @@
 class Train < ApplicationRecord
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
-  belongs_to :route
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, required: false
+  belongs_to :route, required: false
   has_many :tickets
 
   validates :number, presence: true, uniqueness: true

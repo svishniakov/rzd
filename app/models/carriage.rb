e0d921c35b38cad7740f1f9a7bc9264a53313c05
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-14 01:44:15 +0200

    Lesson 16/4-5

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,5 +1,13 @@
 class Carriage < ApplicationRecord
   belongs_to :train
 
-  # validates :number, presence: true
+  validates :number, presence: true
+  validates :number, uniqueness: { scope: :train_id }
+  before_validation :carriage_number
+
+  protected
+
+  def carriage_number
+    train.carriages.empty? ? self.number = 1 : self.number = train.carriages.maximum(:number) + 1
+  end
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,5 @@
 class Train < ApplicationRecord
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, required: false
   belongs_to :route, required: false
   has_many :tickets
   has_many :carriages

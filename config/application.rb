21951ccbfab963e7036b4e6a83f83aed26ea7736
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-26 16:06:22 +0300

    Localization start. Login form localization

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module Rzd
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W[#{config.root}/app/models/carriages]
+
+    # localization settings
+    config.i18n.default_locale = :ru
   end
 end

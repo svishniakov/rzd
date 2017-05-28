d7cc705b99ed22fa0e3fefdfa23517215491f1ae
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-28 17:43:54 +0300

    Sendgrid implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'quiet-spire-91718.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :port           => ENV['MAILGUN_SMTP_PORT'],
-      :address        => ENV['MAILGUN_SMTP_SERVER'],
-      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'quiet-spire-91718.herokuapp.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'quiet-spire-91718.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
       :authentication => :plain,
+      :enable_starttls_auto => true
   }
 end

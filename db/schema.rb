a5e7c03c48adf5146e0c8a674f4b92b1cba37c6f
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-05-28 15:32:02 +0300

    Indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524150659) do
+ActiveRecord::Schema.define(version: 20170528122709) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170524150659) do
     t.integer  "side_bottom_seats"
     t.integer  "sitting_seats"
     t.string   "type"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
@@ -39,6 +40,9 @@ ActiveRecord::Schema.define(version: 20170524150659) do
     t.time    "arrival_time"
     t.time    "departure_time"
     t.index ["position"], name: "index_railway_stations_routes_on_position"
+    t.index ["railway_station_id"], name: "index_railway_stations_routes_on_railway_station_id"
+    t.index ["route_id", "position"], name: "index_railway_stations_routes_on_route_id_and_position", unique: true
+    t.index ["route_id"], name: "index_railway_stations_routes_on_route_id"
   end
 
   create_table "routes", force: :cascade do |t|
